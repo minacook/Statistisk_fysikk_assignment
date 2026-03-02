89704400f4faaeedfe3da09aa969efa090c09f80
minacook <mina.m.v.cook@gmail.com>
2026-03-02 12:50:25 +0100

    Commit before merging task2 and 3

--- Task2.py
+++ Task2.py
@@ -7,7 +7,7 @@ K = 25
 dt = 0.005
 T = 20
 
-def create_init_pos(N,R, min_distance = 0.5):
+def create_init_pos(N,R, min_distance = 1):
     r_arr = np.zeros((N, 2))
     v_arr = np.zeros((N, 2))
     v_arr[:,1] = 5
@@ -70,8 +70,6 @@ def force_working_on_particles(N, R, K, r_arr):
 
     return f
 
-import numpy as np
-
 def calc_tot_energy(pos, vel, t_arr, N=10, R=10):
     E = np.zeros_like(t_arr, dtype=float)
 
@@ -79,15 +77,12 @@ def calc_tot_energy(pos, vel, t_arr, N=10, R=10):
     N = pos.shape[1]
 
     for t in range(Tsteps):
-        # Kinetisk energi: 1/2 sum_i |v_i|^2  (m=1)
         K_t = 0.5 * np.sum(vel[t, :, 0]**2 + vel[t, :, 1]**2)
 
-        # Veggpotensial: sum_i 1/2 K (r_i - R)^2 for r_i > R
-        r = np.linalg.norm(pos[t], axis=1)  # (N,)
+        r = np.linalg.norm(pos[t], axis=1)
         outside = r > R
         V_wall = 0.5 * K * np.sum((r[outside] - R)**2)
 
-        # Lennard-Jones potensial: sum_{i<j} 4[(1/r)^12 - (1/r)^6]
         V_lj = 0.0
         for i in range(N):
             for j in range(i+1, N):
@@ -104,24 +99,26 @@ def calc_tot_energy(pos, vel, t_arr, N=10, R=10):
     return E
 
 def test_code_const_energy():
-    N1, R1, dt1 = 10, 10, 0.005
+    '''N1, R1, dt1 = 10, 10, 0.005
     r_init1, v_init1 = create_init_pos(N1, R1)
     pos_arr1, vel_arr1, t_arr1 = motion_of_particles(N1, R1, K, dt1, r_init1, v_init1, T)
-    E1 = calc_tot_energy(pos_arr1, vel_arr1, t_arr1, N1, R1)
+    E1 = calc_tot_energy(pos_arr1, vel_arr1, t_arr1, N1, R1)'''
 
-    N2, R2, dt2 = 50, 50, 0.005
+    '''N2, R2, dt2 = 40, 50, 0.005
     r_init2, v_init2 = create_init_pos(N2, R2)
     pos_arr2, vel_arr2, t_arr2 = motion_of_particles(N2, R2, K, dt2, r_init2, v_init2, T)
     E2 = calc_tot_energy(pos_arr2, vel_arr2, t_arr2, N2, R2)
 
-    N3, R3, dt3 = 50, 50, 0.001
+    '''
+
+    N3, R3, dt3 = 40, 50, 0.001
     r_init3, v_init3 = create_init_pos(N3, R3)
     pos_arr3, vel_arr3, t_arr3 = motion_of_particles(N3, R3, K, dt3, r_init3, v_init3, T)
     E3 = calc_tot_energy(pos_arr3, vel_arr3, t_arr3, N3, R3)
 
-    plt.plot(t_arr1, E1, label=f'N={N1}, R={R1}, dt={dt1}')
-    plt.plot(t_arr2, E2, label=f'N={N2}, R={R2}, dt={dt2}')
-    plt.plot(t_arr3, E3, label=f'N={N3}, R={R3}, dt={dt3}')
+    #plt.plot(t_arr1, E1, label=f'N={N1}, R={R1}, dt={dt1}', color = 'cornflowerblue')
+    #plt.plot(t_arr2, E2, label=f'N={N2}, R={R2}, dt={dt2}', color= 'darksalmon')
+    plt.plot(t_arr3, E3, label=f'N={N3}, R={R3}, dt={dt3}', color = 'mediumseagreen')
     plt.xlabel('t'); plt.ylabel('E'); plt.grid(alpha=0.2)
     plt.title('Energy over time')
     plt.legend()
